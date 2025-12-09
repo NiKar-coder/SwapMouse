96ad8272a3917db54a78ba497c05aecacebd521d
NiKar-coder <Nikitakarataev22@gmail.com>
2025-12-10 01:04:00 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+class SwapMouseButtons
+{
+    [DllImport("user32.dll")]
+    public static extern int SwapMouseButton(Int32 fSwap);
+
+    static void Main(string[] args)
+    {
+        int alreadyPrimary = SwapMouseButton(1);
+        if (alreadyPrimary != 0)
+        {
+            SwapMouseButton(0);
+        }
+    }
+}
